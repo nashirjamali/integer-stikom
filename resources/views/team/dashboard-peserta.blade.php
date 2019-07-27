c7028f56c02d974d1b5f7535c9f5c2dab2835928
sebastianus sembara <sembara9090@gmail.com>
2019-07-27 12:04:35 +0700

    add content-head

--- resources/views/team/dashboard-peserta.blade.php
+++ resources/views/team/dashboard-peserta.blade.php
@@ -1,7 +1,14 @@
-@extends('team.dashboard-peserta')
+@extends('team.layouts')
 
 @section('content-head')
+<div class="card col-md-12">
+  <div class="card-body">
+    <h5 class="card-title">Nama Team</h5>
+    <h3 class="card-subtitle mb-2 text-muted">Stikom-DEV</h3>
+  </div>
+</div>
 @endsection
 
 @section('content-body')
+
 @endsection
