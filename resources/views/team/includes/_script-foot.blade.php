bb06bc93e92d68707b0ee18fcc15be2ad8d70f3b
sebastianus sembara <sembara9090@gmail.com>
2019-07-28 15:01:54 +0700

    pagination-issue v2

--- resources/views/team/includes/_script-foot.blade.php
+++ resources/views/team/includes/_script-foot.blade.php
@@ -4,11 +4,6 @@
 <script src="{{ asset('assets/argon/vendor/jquery-ui-1.12.1/jquery-ui.min.js') }}"></script>
 <!-- datatable -->
 <script src="{{ asset('/assets/argon/vendor/datatable.js/datatables.min.js') }}" defer></script>
-<script>
-    $(document).ready(function() {
-        $('#list_anggota').DataTable({});
-    });
-</script>
 <!-- bootstrap -->
 <script src="{{ asset('assets/argon/vendor/bootstrap/dist/js/bootstrap.min.js') }}"></script>
 <!-- js files -->
@@ -20,4 +15,16 @@
             token: "ee6fab19c5a04ac1a32a645abde4613a",
             application: "argon-dashboard-free"
         });
+</script>
+<script>
+    let Dashboard = {
+        init: function() {
+            $(document).ready(function() {
+                $('#list_anggota').DataTable({});
+            });
+        }
+    };
+    jQuery(document).ready(function() {
+        Dashboard.init();
+    });
 </script>
