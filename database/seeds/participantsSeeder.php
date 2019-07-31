b14acc87213476a201d0dcfff8824694f7ad27a1
Sebastianus Sembara <sembara9090@gmail.com>
2019-07-31 16:59:45 +0700

    edit data ParticipantsSeeder

--- database/seeds/participantsSeeder.php
+++ database/seeds/participantsSeeder.php
@@ -12,16 +12,43 @@ class ParticipantsSeeder extends Seeder
     public function run()
     {
         DB::table('teams')->insert([
-            'id'   => 1,
-            'name' => 'Sebastianus Sembara',
-            'birth_date' => '1999-01-20',
-            'email' => str_random(10) . '@stikom.edu',
-            'phone' => '089616645409',
-            'tshirt' => 'XL',
-            'vegetarian' => 1,
-            'identity_card' => 'ktm.jpg',
-            'status' => 1,
-            'team_id'=> 1,
+            [
+                'id'   => 1,
+                'name' => 'Sebastianus Sembara',
+                'birth_date' => '1999-01-20',
+                'email' => str_random(10) . '@stikom.edu',
+                'phone' => '089616645409',
+                'tshirt' => 'XL',
+                'vegetarian' => 1,
+                'identity_card' => 'ktm.jpg',
+                'status' => 1,
+                'team_id'=> 1,
+            ],
+            [
+                'id'   => 2,
+                'name' => 'Nashir Jamali',
+                'birth_date' => '1998-06-13',
+                'email' => str_random(10) . '@stikom.edu',
+                'phone' => '089616645409',
+                'tshirt' => 'L',
+                'vegetarian' => 1,
+                'identity_card' => 'ktm2.jpg',
+                'status' => 1,
+                'team_id'=> 1,  
+            ],
+            [
+                'id'   => 3,
+                'name' => 'Ibnu Pratama',
+                'birth_date' => '1999-10-22',
+                'email' => str_random(10) . '@stikom.edu',
+                'phone' => '089616645098',
+                'tshirt' => 'L',
+                'vegetarian' => 1,
+                'identity_card' => 'ktm3.jpg',
+                'status' => 1,
+                'team_id'=> 1,
+            ],
+           
         ]);
     }
 }
