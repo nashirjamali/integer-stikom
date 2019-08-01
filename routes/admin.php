ab19f2f0309950d76d337756c113e012d07d4e24
AyubRW <ayubrizki14@gmail.com>
2019-08-01 13:01:25 +0700

    add list tim

--- routes/admin.php
+++ routes/admin.php
@@ -13,6 +13,14 @@ Route::name('admin.')->group(function () {
             return View::make('admin.dashboard-admin');
         })->name('dashboard-admin');
 
+        Route::get('admin/list-tim', function () {
+            return View::make('admin.list-tim');
+        })->name('list-tim');
+
+        Route::get('admin/admin-submission', function () {
+            return View::make('admin.admin-submission');
+        })->name('admin-submission');
+        
         Route::get('admin/admin-detail', function () {
             return View::make('admin.admin-detail');
         })->name('admin-detail');
