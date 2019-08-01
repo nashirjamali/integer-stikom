ea91e93674324ab7e584b79fa399c8b633b4aa1f
Sebastianus Sembara <sembara9090@gmail.com>
2019-08-01 08:25:37 +0700

    update route teams

--- routes/team.php
+++ routes/team.php
@@ -13,9 +13,17 @@ Route::name('team.')->group(function () {
             return View::make('team.dashboard-peserta');
         })->name('dashboard-peserta');
 
+        Route::get('team/payments', function () {
+            return View::make('team.payments');
+        })->name('payments');
+
         Route::get('team/submission', function () {
             return View::make('team.submission');
         })->name('submission');
+
+        Route::get('team/video', function () {
+            return View::make('team.video');
+        })->name('video');
         
     });
 });
