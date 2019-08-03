3c4ee7f61a6e4abeb29dca92466358e347f8db98
Sebastianus Sembara <sembara9090@gmail.com>
2019-08-03 08:47:15 +0700

    delete old routes in teams

--- routes/team.php
+++ routes/team.php
@@ -13,22 +13,5 @@ Route::name('team.')->group(function () {
         Route::get( 'team/submission', 'TeamController@submission' );
         Route::get( 'team/video', 'TeamController@video' );
 
-
-        // Route::get('team', function () {
-        //     return View::make('team.dashboard-peserta');
-        // })->name('dashboard-peserta');
-
-        // Route::get('team/payments', function () {
-        //     return View::make('team.payments');
-        // })->name('payments');
-
-        // Route::get('team/submission', function () {
-        //     return View::make('team.submission');
-        // })->name('submission');
-
-        // Route::get('team/video', function () {
-        //     return View::make('team.video');
-        // })->name('video');
-        
     });
 });
