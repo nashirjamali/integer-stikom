9429bf0b33dcd94d15db8ccf0be03e85400e6eb3
ibnuprtma <pibnu86@gmail.com>
2019-08-07 16:48:47 +0700

    remove icon photos user

--- resources/views/admin/includes/_navbar.blade.php
+++ resources/views/admin/includes/_navbar.blade.php
@@ -8,11 +8,8 @@
       <li class="nav-item dropdown">
         <a class="nav-link pr-0" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           <div class="media align-items-center">
-            <span class="avatar avatar-sm rounded-circle">
-              <img alt="Image placeholder" src="{{ asset('/assets/argon/img/theme/team-4-800x800.jpg') }}">
-            </span>
             <div class="media-body ml-2 d-none d-lg-block">
-              <span class="mb-0 text-sm  font-weight-bold">{{ Auth::user()->name }}</span>
+              <span class="mb-0 text-sm  font-weight-bold">{{ strtoupper(Auth::user()->name) }}</span>
             </div>
           </div>
         </a>
