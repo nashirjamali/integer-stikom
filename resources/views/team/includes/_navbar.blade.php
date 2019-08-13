7863b739971fe39bc60c8709e312910462f6f211
ibnuprtma <pibnu86@gmail.com>
2019-08-13 15:08:20 +0700

    fix info

--- resources/views/team/includes/_navbar.blade.php
+++ resources/views/team/includes/_navbar.blade.php
@@ -15,24 +15,8 @@
         </a>
         <div class="dropdown-menu dropdown-menu-arrow dropdown-menu-right">
           <div class=" dropdown-header noti-title">
-            <h6 class="text-overflow m-0">Welcome!</h6>
+            <h6 class="text-overflow m-0">Welcome, {{ strtoupper(Auth::user()->name) }}!</h6>
           </div>
-          <a href="./examples/profile.html" class="dropdown-item">
-            <i class="ni ni-single-02"></i>
-            <span>My profile</span>
-          </a>
-          <a href="./examples/profile.html" class="dropdown-item">
-            <i class="ni ni-settings-gear-65"></i>
-            <span>Settings</span>
-          </a>
-          <a href="./examples/profile.html" class="dropdown-item">
-            <i class="ni ni-calendar-grid-58"></i>
-            <span>Activity</span>
-          </a>
-          <a href="./examples/profile.html" class="dropdown-item">
-            <i class="ni ni-support-16"></i>
-            <span>Support</span>
-          </a>
           <div class="dropdown-divider"></div>
           <a class="dropdown-item" href="{{ route('logout') }}"  onclick="event.preventDefault();
               document.getElementById('logout-form').submit();"> <i class="ni ni-user-run"></i>
