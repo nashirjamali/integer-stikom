666caa67635827ca2cf24e27102fadfb0a4a31bb
ibnuprtma <pibnu86@gmail.com>
2019-08-17 14:29:25 +0700

    change to udin

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Integer Stikom 2019</title>
+    <title>Integer Universitas Dinamika 2019</title>
 
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,600,700" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,700i" rel="stylesheet">
