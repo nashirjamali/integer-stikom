84afe604c7a37b875207af9546f82a41ad14da8c
Sebastianus Sembara <sembara9090@gmail.com>
2019-08-19 18:02:22 +0700

    view dashboard edit

--- resources/views/dashboard-template.blade.php
+++ resources/views/dashboard-template.blade.php
@@ -25,17 +25,15 @@
 <body id="page-top">
 
     <nav class="navbar navbar-expand navbar-dark bg-dark static-top">
-
-        <a class="navbar-brand mr-1" href="/">Logo</a>
-
-        <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
+        <a class="navbar-brand mr-1" href="/"><img src="{{ asset('images\logo.png') }}"></a>
+        <!-- Navbar -->
+        <button class="btn btn-link btn-sm text-white order-1 order-sm-0 ml-5" id="sidebarToggle" href="#">
             <i class="fas fa-bars"></i>
         </button>
-        <!-- Navbar -->
         <ul class="d-none d-md-inline-block navbar-nav ml-auto mr-0 mr-md-3 my-2 my-md-0">
             <li class="nav-item dropdown no-arrow">
                 <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <i class="fas fa-user-circle fa-fw"></i>
+                    <b><span>hai, {{ Auth::user()->name }}</span></b>
                 </a>
                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
                     <div class="dropdown-divider"></div>
@@ -49,8 +47,8 @@
                 </div>
             </li>
         </ul>
-
     </nav>
+    <!-- navbar -->
 
     <div id="wrapper">
 
